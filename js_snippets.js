d12e06adb9e7a14498125146ff4eb67449a9817d
CarlosPimentel10 <carlos.costa.pimentel@gmail.com>
2020-01-08 16:53:19 +0100

    Solved the convenience store JS challenge

--- js_snippets.js
+++ js_snippets.js
@@ -185,3 +185,18 @@ function arrayOfMultiples(num, length) {
 function objectToArray(obj) {
 	return Object.entries(obj);
 }
+/*
+Given a total due and an array representing 
+the amount of change in your pocket, determine whether or not you are able to pay for the item. 
+Change will always be represented in the following order: quarters, dimes, nickels, pennies.
+To illustrate: changeEnough([25, 20, 5, 0], 4.25) 
+should yield true, since having 25 quarters, 20 dimes, 5 nickels and 0 pennies gives you 6.25 + 2 + .25 + 0 = 8.50.
+*/
+function changeEnough(change, amountDue) {
+	accum = change[0] * 0.25 + change[1] * 0.1 + change[2] * 0.05 + change[3] * 0.01;
+	if (accum >= amountDue) {
+		return true;
+	} else {
+		return false;
+	}
+}
